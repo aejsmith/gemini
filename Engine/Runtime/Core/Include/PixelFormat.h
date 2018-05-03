bbb707066b1318b0a4f6fda4c2ec95d962c0c575
Alex Smith <alex@alex-smith.me.uk>
2018-05-03 17:02:42 +0100

    Add PixelFormat class

---
+++ Engine/Runtime/Core/Include/PixelFormat.h
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2018 Alex Smith
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#pragma once
+
+#include "Core/CoreDefs.h"
+
+class PixelFormat
+{
+public:
+    enum Impl
+    {
+        /** Unknown format. */
+        kUnknown,
+
+        /**
+         * Colour formats. Note these are all given in the order of elements in
+         * memory, independent of endianness.
+         */
+        kR8G8B8A8,              /**< RGBA, unsigned normalized, 8 bits per component. */
+        kR8G8B8A8sRGB,          /**< RGBA, unsigned normalized, 8 bits per component, sRGB. */
+        kR8G8,                  /**< RG, unsigned normalized, 8 bits per component. */
+        kR8,                    /**< R, unsigned normalized, 8 bits per component. */
+        kB8G8R8A8,              /**< BGRA, unsigned normalized, 8 bits per component. */
+        kB8G8R8A8sRGB,          /**< BGRA, unsigned normalized, 8 bits per component, sRGB. */
+        kR10G10B10A2,           /**< RGBA, unsigned normalized, 10 bits RGB, 2 bits A. */
+        kFloatR16G16B16A16,     /**< RGBA, float, 16 bits per component. */
+        kFloatR16G16B16,        /**< RGB, float, 16 bits per component. */
+        kFloatR16G16,           /**< RG, float, 16 bits per component. */
+        kFloatR16,              /**< R, float, 16 bits per component. */
+        kFloatR32G32B32A32,     /**< RGBA, float, 32 bits per component. */
+        kFloatR32G32B32,        /**< RGB, float, 32 bits per component. */
+        kFloatR32G32,           /**< RG, float, 32 bits per component. */
+        kFloatR32,              /**< R, float, 32 bits per component. */
+
+        /** Depth/stencil formats. */
+        kDepth16,               /**< Depth, 16 bits. */
+        kDepth32,               /**< Depth, 32 bits. */
+        kDepth32Stencil8,       /**< Depth/stencil, 24 bits/8 bits. */
+
+        /** Number of pixel formats. */
+        kNumFormats,
+    };
+
+    struct Info
+    {
+        bool                    isSRGB;
+        bool                    isFloat;
+        bool                    isDepth;
+        bool                    isDepthStencil;
+        size_t                  bytesPerPixel;
+    };
+
+public:
+    constexpr                   PixelFormat() : mValue(kUnknown) {}
+    constexpr                   PixelFormat(const Impl inValue) : mValue(inValue) {}
+    constexpr                   operator Impl() const { return mValue; }
+
+    static bool                 IsColour(const PixelFormat inFormat);
+    static bool                 IsSRGB(const PixelFormat inFormat);
+    static bool                 IsFloat(const PixelFormat inFormat);
+    static bool                 IsDepth(const PixelFormat inFormat);
+    static bool                 IsDepthStencil(const PixelFormat inFormat);
+    static size_t               BytesPerPixel(const PixelFormat inFormat);
+
+    static PixelFormat          GetSRGBEquivalent(const PixelFormat inFormat);
+    static PixelFormat          GetNonSRGBEquivalent(const PixelFormat inFormat);
+
+private:
+    Impl                        mValue;
+
+    static Info                 sInfo[kNumFormats];
+
+};
+
+inline bool PixelFormat::IsColour(const PixelFormat inFormat)
+{
+    return !sInfo[inFormat].isDepth;
+}
+
+inline bool PixelFormat::IsSRGB(const PixelFormat inFormat)
+{
+    return sInfo[inFormat].isSRGB;
+}
+
+inline bool PixelFormat::IsFloat(const PixelFormat inFormat)
+{
+    return sInfo[inFormat].isFloat;
+}
+
+inline bool PixelFormat::IsDepth(const PixelFormat inFormat)
+{
+    return sInfo[inFormat].isDepth;
+}
+
+inline bool PixelFormat::IsDepthStencil(const PixelFormat inFormat)
+{
+    return sInfo[inFormat].isDepthStencil;
+}
+
+inline size_t PixelFormat::BytesPerPixel(const PixelFormat inFormat)
+{
+    return sInfo[inFormat].bytesPerPixel;
+}
---
+++ Engine/Runtime/Core/Source/PixelFormat.cpp
@@ -0,0 +1,223 @@
+/*
+ * Copyright (C) 2018 Alex Smith
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "Core/PixelFormat.h"
+
+PixelFormat::Info PixelFormat::sInfo[PixelFormat::kNumFormats] =
+{
+    /* kUnknown */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ false,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 0,
+    },
+
+    /* kR8G8B8A8 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ false,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 4,
+    },
+
+    /* kR8G8B8A8sRGB */
+    {
+        /* isSRGB         */ true,
+        /* isFloat        */ false,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 4,
+    },
+
+    /* kR8G8 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ false,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 2,
+    },
+
+    /* kR8 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ false,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 1,
+    },
+
+    /* kB8G8R8A8 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ false,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 4,
+    },
+
+    /* kB8G8R8A8sRGB */
+    {
+        /* isSRGB         */ true,
+        /* isFloat        */ false,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 4,
+    },
+
+    /* kR10G10B10A2 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ false,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 4,
+    },
+
+    /* kFloatR16G16B16A16 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ true,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 8,
+    },
+
+    /* kFloatR16G16B16 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ true,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 6,
+    },
+
+    /* kFloatR16G16 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ true,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 4,
+    },
+
+    /* kFloatR16 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ true,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 2,
+    },
+
+    /* kFloatR32G32B32A32 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ true,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 16,
+    },
+
+    /* kFloatR32G32B32 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ true,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 12,
+    },
+
+    /* kFloatR32G32 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ true,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 8,
+    },
+
+    /* kFloatR32 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ true,
+        /* isDepth        */ false,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 4,
+    },
+
+    /* kDepth16 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ false,
+        /* isDepth        */ true,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 2,
+    },
+
+    /* kDepth32 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ false,
+        /* isDepth        */ true,
+        /* isDepthStencil */ false,
+        /* bytesPerPixel  */ 4,
+    },
+
+    /* kDepth32Stencil8 */
+    {
+        /* isSRGB         */ false,
+        /* isFloat        */ false,
+        /* isDepth        */ true,
+        /* isDepthStencil */ true,
+        /* bytesPerPixel  */ 8,
+    }
+};
+
+PixelFormat PixelFormat::GetSRGBEquivalent(const PixelFormat inFormat)
+{
+    switch (inFormat)
+    {
+        case PixelFormat::kR8G8B8A8:
+            return PixelFormat::kR8G8B8A8sRGB;
+
+        case PixelFormat::kB8G8R8A8:
+            return PixelFormat::kB8G8R8A8sRGB;
+
+        default:
+            return inFormat;
+
+    }
+}
+
+PixelFormat PixelFormat::GetNonSRGBEquivalent(const PixelFormat inFormat)
+{
+    switch (inFormat)
+    {
+        case PixelFormat::kR8G8B8A8sRGB:
+            return PixelFormat::kR8G8B8A8;
+
+        case PixelFormat::kB8G8R8A8sRGB:
+            return PixelFormat::kB8G8R8A8;
+
+        default:
+            return inFormat;
+
+    }
+}
