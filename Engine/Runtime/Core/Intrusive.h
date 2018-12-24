58857d0aa0ff7463bc9e2791b4d094af160c937b
Alex Smith <alex@alex-smith.me.uk>
2018-12-24 18:10:14 +0000

    Add IntrusiveList container class

---
+++ Engine/Runtime/Core/Intrusive.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2018 Alex Smith
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#pragma once
+
+#include "Core/CoreDefs.h"
+
+/**
+ * Helper functions for implementation of intrusive containers, where the link
+ * to the container is embedded within the objects being stored in the container
+ * rather than allocated separately.
+ */
+template <typename T, typename Node, Node T::*Member>
+class IntrusiveImpl
+{
+public:
+    /** Given a node pointer, return a pointer to the object containing it. */
+    static T*               GetValue(Node* const inNode);
+    static const T*         GetValue(const Node* const inNode);
+
+    /** Given a value pointer, return a pointer to its node. */
+    static Node*            GetNode(T* const inValue);
+    static const Node*      GetNode(const T* const inValue);
+};
+
+template <typename T, typename Node, Node T::*Member>
+inline T* IntrusiveImpl<T, Node, Member>::GetValue(Node* const inNode)
+{
+    Node* const offset = &(reinterpret_cast<T*>(0)->*Member);
+    return reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(inNode) - reinterpret_cast<uintptr_t>(offset));
+}
+
+template <typename T, typename Node, Node T::*Member>
+inline const T* IntrusiveImpl<T, Node, Member>::GetValue(const Node* const inNode)
+{
+    return const_cast<const T*>(GetValue(const_cast<Node*>(inNode)));
+}
+
+template <typename T, typename Node, Node T::*Member>
+inline Node* IntrusiveImpl<T, Node, Member>::GetNode(T* const inValue)
+{
+    return &(inValue->*Member);
+}
+
+template <typename T, typename Node, Node T::*Member>
+inline const Node* IntrusiveImpl<T, Node, Member>::GetNode(const T* const inValue)
+{
+    return &(inValue->*Member);
+}
---
+++ Engine/Runtime/Core/IntrusiveList.h
@@ -0,0 +1,364 @@
+/*
+ * Copyright (C) 2018 Alex Smith
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#pragma once
+
+#include "Core/Intrusive.h"
+
+/**
+ * Node for an intrusive list. Usage is to embed this inside the type being
+ * stored in a list and point to it with IntrusiveList's node template
+ * parameter.
+ *
+ * Note that copying a node (e.g. by copying the object containing it) does not
+ * copy or change membership: the copy constructor initialises the node as not
+ * attached to any list, while the copy assignment operator does not do anything
+ * (leaves the destination attached to any list it is currently attached to).
+ */
+class IntrusiveListNode
+{
+public:
+                                IntrusiveListNode();
+                                IntrusiveListNode(const IntrusiveListNode& inOther);
+                                ~IntrusiveListNode();
+
+    IntrusiveListNode&          operator=(const IntrusiveListNode& inOther);
+
+    bool                        IsInserted() const;
+
+private:
+    IntrusiveListNode*          mPrevious;
+    IntrusiveListNode*          mNext;
+
+    template <typename T, IntrusiveListNode T::*Member>
+    friend class IntrusiveList;
+};
+
+inline IntrusiveListNode::IntrusiveListNode() :
+    mPrevious   (this),
+    mNext       (this)
+{
+}
+
+inline IntrusiveListNode::IntrusiveListNode(const IntrusiveListNode& inOther) :
+    mPrevious   (this),
+    mNext       (this)
+{
+}
+
+inline IntrusiveListNode::~IntrusiveListNode()
+{
+    Assert(!IsInserted());
+}
+
+inline bool IntrusiveListNode::IsInserted() const
+{
+    return mNext != this;
+}
+
+/**
+ * Intrusive doubly-linked list class. Stores the node as part of the objects
+ * contained in the list, which means no additional allocation needs to be
+ * performed by the container, and also allows constant-time removal of objects
+ * from the list given a pointer to the object.
+ *
+ * Note that while the list provides a copy constructor/assignment operator,
+ * they do not copy the list content, as objects only have one node and can
+ * therefore only be in one list at a time. The move constructor/assignment
+ * operator do, however, move the list content and leave the original list
+ * empty.
+ */
+template <typename T, IntrusiveListNode T::*Member>
+class IntrusiveList
+{
+public:
+                                IntrusiveList();
+                                IntrusiveList(const IntrusiveList& inOther);
+                                IntrusiveList(IntrusiveList&& inOther);
+                                ~IntrusiveList();
+
+    IntrusiveList&              operator=(const IntrusiveList& inOther);
+    IntrusiveList&              operator=(IntrusiveList&& inOther);
+
+    bool                        IsEmpty() const;
+
+    void                        Append(T* const inValue);
+    void                        Prepend(T* const inValue);
+    void                        InsertBefore(T* const inPosition,
+                                             T* const inValue);
+    void                        InsertAfter(T* const inPosition,
+                                            T* const inValue);
+
+    void                        Remove(T* const inValue);
+    T*                          RemoveFirst();
+    T*                          RemoveLast();
+    void                        Clear();
+
+    T*                          First() const;
+    T*                          Last() const;
+
+    /**
+     * Return a pointer to the preceding/following element in the list, or null
+     * if the element is the first/last element, respectively.
+     */
+    T*                          Previous(T* const inCurrent) const;
+    const T*                    Previous(const T* const inCurrent) const;
+    T*                          Next(T* const inCurrent) const;
+    const T*                    Next(const T* const inCurrent) const;
+
+private:
+    using Impl                = IntrusiveImpl<T, IntrusiveListNode, Member>;
+
+private:
+    static void                 InsertBefore(IntrusiveListNode* const inPosition,
+                                             IntrusiveListNode* const inNode);
+    static void                 InsertAfter(IntrusiveListNode* const inPosition,
+                                            IntrusiveListNode* const inNode);
+    static void                 Remove(IntrusiveListNode* const inNode);
+
+private:
+    IntrusiveListNode           mHead;
+
+};
+
+#define INTRUSIVELIST_TEMPLATE \
+    template <typename T, IntrusiveListNode T::*Member>
+
+INTRUSIVELIST_TEMPLATE
+inline IntrusiveList<T, Member>::IntrusiveList()
+{
+}
+
+INTRUSIVELIST_TEMPLATE
+inline IntrusiveList<T, Member>::IntrusiveList(const IntrusiveList& inOther)
+{
+}
+
+INTRUSIVELIST_TEMPLATE
+inline IntrusiveList<T, Member>::IntrusiveList(IntrusiveList&& inOther)
+{
+    mHead.mPrevious = inOther.mHead.mPrevious;
+    mHead.mNext     = inOther.mHead.mNext;
+
+    inOther.mHead.mPrevious = &inOther.mHead;
+    inOther.mHead.mNext     = &inOther.mHead;
+}
+
+INTRUSIVELIST_TEMPLATE
+inline IntrusiveList<T, Member>::~IntrusiveList()
+{
+    Assert(IsEmpty());
+}
+
+INTRUSIVELIST_TEMPLATE
+inline IntrusiveList<T, Member>& IntrusiveList<T, Member>::operator=(const IntrusiveList& inOther)
+{
+}
+
+INTRUSIVELIST_TEMPLATE
+inline IntrusiveList<T, Member>& IntrusiveList<T, Member>::operator=(IntrusiveList&& inOther)
+{
+    Clear();
+
+    mHead.mPrevious = inOther.mHead.mPrevious;
+    mHead.mNext     = inOther.mHead.mNext;
+
+    inOther.mHead.mPrevious = &inOther.mHead;
+    inOther.mHead.mNext     = &inOther.mHead;
+}
+
+INTRUSIVELIST_TEMPLATE
+inline bool IntrusiveList<T, Member>::IsEmpty() const
+{
+    return !mHead.IsInserted();
+}
+
+INTRUSIVELIST_TEMPLATE
+inline void IntrusiveList<T, Member>::Append(T* const inValue)
+{
+    InsertBefore(&mHead, Impl::GetNode(inValue));
+}
+
+INTRUSIVELIST_TEMPLATE
+inline void IntrusiveList<T, Member>::Prepend(T* const inValue)
+{
+    InsertAfter(&mHead, Impl::GetNode(inValue));
+}
+
+INTRUSIVELIST_TEMPLATE
+inline void IntrusiveList<T, Member>::InsertBefore(T* const inPosition, T* const inValue)
+{
+    InsertBefore(Impl::GetNode(inPosition), Impl::GetNode(inValue));
+}
+
+INTRUSIVELIST_TEMPLATE
+inline void IntrusiveList<T, Member>::InsertAfter(T* const inPosition, T* const inValue)
+{
+    InsertAfter(Impl::GetNode(inPosition), Impl::GetNode(inValue));
+}
+
+INTRUSIVELIST_TEMPLATE
+inline void IntrusiveList<T, Member>::Remove(T* const inValue)
+{
+    Remove(Impl::GetNode(inValue));
+}
+
+INTRUSIVELIST_TEMPLATE
+inline T* IntrusiveList<T, Member>::RemoveFirst()
+{
+    T* const value = First();
+
+    if (value)
+    {
+        Remove(value);
+    }
+
+    return value;
+}
+
+INTRUSIVELIST_TEMPLATE
+inline T* IntrusiveList<T, Member>::RemoveLast()
+{
+    T* const value = Last();
+
+    if (value)
+    {
+        Remove(value);
+    }
+
+    return value;
+}
+
+INTRUSIVELIST_TEMPLATE
+inline void IntrusiveList<T, Member>::Clear()
+{
+    while (!IsEmpty())
+    {
+        RemoveFirst();
+    }
+}
+
+INTRUSIVELIST_TEMPLATE
+inline T* IntrusiveList<T, Member>::First() const
+{
+    return Impl::GetValue(mHead.mNext);
+}
+
+INTRUSIVELIST_TEMPLATE
+inline T* IntrusiveList<T, Member>::Last() const
+{
+    return Impl::GetValue(mHead.mPrevious);
+}
+
+INTRUSIVELIST_TEMPLATE
+inline T* IntrusiveList<T, Member>::Previous(T* const inCurrent) const
+{
+    IntrusiveListNode* node = Impl::GetNode(inCurrent);
+
+    if (node->mPrevious != &mHead)
+    {
+        return Impl::GetValue(node->mPrevious);
+    }
+    else
+    {
+        return nullptr;
+    }
+}
+
+INTRUSIVELIST_TEMPLATE
+inline const T* IntrusiveList<T, Member>::Previous(const T* const inCurrent) const
+{
+    IntrusiveListNode* node = Impl::GetNode(inCurrent);
+
+    if (node->mPrevious != &mHead)
+    {
+        return Impl::GetValue(node->mPrevious);
+    }
+    else
+    {
+        return nullptr;
+    }
+}
+
+INTRUSIVELIST_TEMPLATE
+inline T* IntrusiveList<T, Member>::Next(T* const inCurrent) const
+{
+    IntrusiveListNode* node = Impl::GetNode(inCurrent);
+
+    if (node->mNext != &mHead)
+    {
+        return Impl::GetValue(node->mNext);
+    }
+    else
+    {
+        return nullptr;
+    }
+}
+
+INTRUSIVELIST_TEMPLATE
+inline const T* IntrusiveList<T, Member>::Next(const T* const inCurrent) const
+{
+    IntrusiveListNode* node = Impl::GetNode(inCurrent);
+
+    if (node->mNext != &mHead)
+    {
+        return Impl::GetValue(node->mNext);
+    }
+    else
+    {
+        return nullptr;
+    }
+}
+
+INTRUSIVELIST_TEMPLATE
+inline void IntrusiveList<T, Member>::InsertBefore(IntrusiveListNode* const inPosition,
+                                                   IntrusiveListNode* const inNode)
+{
+    Assert(!inNode->IsInserted());
+
+    inNode->mPrevious = inPosition->mPrevious;
+    inNode->mNext     = inPosition;
+
+    inPosition->mPrevious->mNext = inNode;
+    inPosition->mPrevious        = inNode;
+}
+
+INTRUSIVELIST_TEMPLATE
+inline void IntrusiveList<T, Member>::InsertAfter(IntrusiveListNode* const inPosition,
+                                                  IntrusiveListNode* const inNode)
+{
+    Assert(!inNode->IsInserted());
+
+    inNode->mPrevious = inPosition;
+    inNode->mNext     = inPosition->mNext;
+
+    inPosition->mNext->mPrevious = inNode;
+    inPosition->mNext            = inNode;
+}
+
+INTRUSIVELIST_TEMPLATE
+inline void IntrusiveList<T, Member>::Remove(IntrusiveListNode* const inNode)
+{
+    Assert(inNode->IsInserted());
+
+    inNode->mPrevious->mNext = inNode->mNext;
+    inNode->mNext->mPrevious = inNode->mPrevious;
+
+    inNode->mNext     = inNode;
+    inNode->mPrevious = inNode;
+}
+
+#undef INTRUSIVELIST_TEMPLATE
