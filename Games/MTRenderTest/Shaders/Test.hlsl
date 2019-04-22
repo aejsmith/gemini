53f553996e198e1df304939079cc9dda7e23e6d9
Alex Smith <alex@alex-smith.me.uk>
2019-04-22 10:41:26 +0100

    Fork current multithreaded rendering test to a MTRenderTest app

---
+++ Games/MTRenderTest/Shaders/Test.hlsl
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2018-2019 Alex Smith
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+struct VSInput
+{
+    float2      position    : POSITION;
+    float4      colour      : COLOR;
+};
+
+struct PSInput
+{
+    float4      position    : SV_POSITION;
+    float4      colour      : COLOR;
+};
+
+cbuffer Constants : register(b0, space0)
+{
+    float4x4    transform;
+};
+
+PSInput VSMain(VSInput input)
+{
+    PSInput output;
+    output.position = mul(transform, float4(input.position, 0.0, 1.0));
+    output.colour   = input.colour;
+    return output;
+}
+
+float4 PSMain(PSInput input) : SV_TARGET
+{
+    return input.colour;
+}
