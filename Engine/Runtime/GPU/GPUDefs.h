aejsmith/gemini

commit e747b9d5d021bb9467d32ed9f52ace223eb6ffed

Add a shortcut function to add a render graph blit pass

Engine/Runtime/GPU/GPUDefs.h
@@ -148,9 +148,35 @@ struct GPUSubresourceRange
     uint32_t                    layerCount;
 
 public:
+                                GPUSubresourceRange(const uint32_t inMipOffset = 0,
+                                                    const uint32_t inMipCount = 1,
+                                                    const uint32_t inLayerOffset = 0,
+                                                    const uint32_t inLayerCount = 1);
+
+                                GPUSubresourceRange(const GPUSubresource inSubresource);
+
     bool                        Overlaps(const GPUSubresourceRange& inOther) const;
 
 };
+
+inline GPUSubresourceRange::GPUSubresourceRange(const uint32_t inMipOffset,
+                                                const uint32_t inMipCount,
+                                                const uint32_t inLayerOffset,
+                                                const uint32_t inLayerCount) :
+    mipOffset   (inMipOffset),
+    mipCount    (inMipCount),
+    layerOffset (inLayerOffset),
+    layerCount  (inLayerCount)
+{
+}
+
+inline GPUSubresourceRange::GPUSubresourceRange(const GPUSubresource inSubresource) :
+    mipOffset   (inSubresource.mipLevel),
+    mipCount    (1),
+    layerOffset (inSubresource.layer),
+    layerCount  (1)
+{
+}
 
 inline bool GPUSubresourceRange::Overlaps(const GPUSubresourceRange& inOther) const
 {

Engine/Runtime/Render/BasicRenderPipeline.cpp
@@ -116,24 +116,10 @@ void BasicRenderPipeline::Render(const RenderWorld&         inWorld,
     });
 
     /* Blit to the final output. */
-    RenderGraphPass& blitPass = inGraph.AddPass("BasicBlit", kRenderGraphPassType_Transfer);
-
-    blitPass.UseResource(colourTexture,
-                         GPUSubresourceRange{0, 1, 0, 1},
-                         kGPUResourceState_TransferRead,
-                         nullptr);
-    blitPass.UseResource(inTexture,
-                         GPUSubresourceRange{0, 1, 0, 1},
-                         kGPUResourceState_TransferWrite,
-                         &outNewTexture);
-
-    blitPass.SetFunction([colourTexture, inTexture] (const RenderGraph&     inGraph,
-                                                     const RenderGraphPass& inPass,
-                                                     GPUTransferContext&    inContext)
-    {
-        inContext.BlitTexture(inGraph.GetTexture(inTexture),
-                              GPUSubresource{0, 0},
-                              inGraph.GetTexture(colourTexture),
-                              GPUSubresource{0, 0});
-    });
+    inGraph.AddBlitPass("BasicBlit",
+                        inTexture,
+                        GPUSubresource{0, 0},
+                        colourTexture,
+                        GPUSubresource{0, 0},
+                        &outNewTexture);
 }

Engine/Runtime/Render/RenderGraph.cpp
@@ -330,6 +330,42 @@ RenderGraphPass& RenderGraph::AddPass(std::string               inName,
     return *pass;
 }
 
+RenderGraphPass& RenderGraph::AddBlitPass(std::string                inName,
+                                          const RenderResourceHandle inDestHandle,
+                                          const GPUSubresource       inDestSubresource,
+                                          const RenderResourceHandle inSourceHandle,
+                                          const GPUSubresource       inSourceSubresource,
+                                          RenderResourceHandle*      outNewHandle)
+{
+    Assert(GetResourceType(inDestHandle) == kRenderResourceType_Texture);
+    Assert(GetResourceType(inSourceHandle) == kRenderResourceType_Texture);
+    Assert(outNewHandle);
+
+    RenderGraphPass& pass = AddPass(std::move(inName), kRenderGraphPassType_Transfer);
+
+    pass.UseResource(inSourceHandle,
+                     inSourceSubresource,
+                     kGPUResourceState_TransferRead,
+                     nullptr);
+
+    pass.UseResource(inDestHandle,
+                     inDestSubresource,
+                     kGPUResourceState_TransferWrite,
+                     outNewHandle);
+
+    pass.SetFunction([=] (const RenderGraph&     inGraph,
+                          const RenderGraphPass& inPass,
+                          GPUTransferContext&    inContext)
+    {
+        inContext.BlitTexture(inGraph.GetTexture(inDestHandle),
+                              inDestSubresource,
+                              inGraph.GetTexture(inSourceHandle),
+                              inSourceSubresource);
+    });
+
+    return pass;
+}
+
 RenderGraph::Resource::Resource() :
     texture         (),
     usage           (kGPUResourceUsage_Standard),

Engine/Runtime/Render/RenderGraph.h
@@ -390,6 +390,14 @@ public:
     RenderGraphPass&                AddPass(std::string               inName,
                                             const RenderGraphPassType inType);
 
+    /** Shortcut to add a pass to just blit one texture subresource to another. */
+    RenderGraphPass&                AddBlitPass(std::string                inName,
+                                                const RenderResourceHandle inDestHandle,
+                                                const GPUSubresource       inDestSubresource,
+                                                const RenderResourceHandle inSourceHandle,
+                                                const GPUSubresource       inSourceSubresource,
+                                                RenderResourceHandle*      outNewHandle);
+
     /**
      * Create a new buffer resource. The initial content will be undefined so
      * the first pass that uses it must write to it.

commit 82c03c7435c95bf800827b012117c450460d8057

Fix default values for GPUSubresourceRange constructor
Commit e747b9d5d0 caused a regression: specifying the subresource counts
as 0 in GPUSubresourceRange is supposed to be a shortcut to refer to the
whole texture. The default constructor arguments were setting to 1, where
they would have been default 0 before that commit.

Engine/Runtime/GPU/GPUDefs.h
@@ -140,6 +140,11 @@ struct GPUSubresource
     uint32_t                    layer;
 };
 
+/**
+ * Structure describing a range of subresources in a texture resource. When
+ * both count values are 0 (as per the default constructor arguments), the
+ * range refers to the whole texture.
+ */
 struct GPUSubresourceRange
 {
     uint32_t                    mipOffset;
@@ -149,9 +154,9 @@ struct GPUSubresourceRange
 
 public:
                                 GPUSubresourceRange(const uint32_t inMipOffset = 0,
-                                                    const uint32_t inMipCount = 1,
+                                                    const uint32_t inMipCount = 0,
                                                     const uint32_t inLayerOffset = 0,
-                                                    const uint32_t inLayerCount = 1);
+                                                    const uint32_t inLayerCount = 0);
 
                                 GPUSubresourceRange(const GPUSubresource inSubresource);
 